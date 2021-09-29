1d8b3062807ab1e019421e7471b359658328c09a
Harshitanaik@26 <naikharshita93@gmail.com>
2021-09-29 19:46:49 +0530

    Student create and show page

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Books = require('../models/book')
+const Student = require('../models/student')
 
 
 //New Route
--- routes/students.js
+++ routes/students.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Student = require('../models/student')
+const Book = require('../models/book')
 const router = express.Router()
 
 //New Student
@@ -7,6 +8,39 @@ router.get('/new',(req,res)=>{
     res.render('students/new',{
         student:new Student()
     })
+})
+
+//Create Student
+router.post('/',async(req,res)=>{
+    const student = new Student({
+        name:req.body.name,
+        rno:req.body.rno,
+        department:req.body.department
+    })
+    try{
+        const newStudent = await student.save()
+        res.redirect(`students/${newStudent.id}`)
+        //res.redirect('/')
+    }catch{
+        res.render('students/new',{
+            student:student
+            //errorMessage:"Something went wrong!"
+        })
+    }
+})
+
+//Show page
+router.get('/:id',async(req,res)=>{
+    try{
+        const student = await Student.findById(req.params.id)
+        const books = await Book.find({student:student.id}).limit(6).exec()
+        res.render('students/show',{
+            student:student,
+            booksAccessed:books
+        })
+    }catch{
+        res.redirect('/')
+    }
 })
 
 module.exports = router
